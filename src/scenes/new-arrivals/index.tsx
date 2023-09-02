42f2d95d07859e089058224cc876a2e9033e6586
Kanchana Walagambahu <kanchana.walagambahu@axiatadigitallabs.com>
2023-09-02 12:29:03 +0530

    favourites done

--- src/scenes/new-arrivals/index.tsx
+++ src/scenes/new-arrivals/index.tsx
@@ -1,10 +1,9 @@
 import HText from '@/shared/HText';
 import { NewArrivalType, SelectedPage } from '@/shared/types';
-import { HomeModernIcon,UserGroupIcon,AcademicCapIcon } from '@heroicons/react/24/solid';
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
 import NewArrival from './NewArrival';
 import ActionButton from '@/shared/ActionButton';
-import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png"
+import SaleImage from "@/assets/saleImage.png"
 import Image1 from '@/assets/NW-Image1.png'
 import Image2 from '@/assets/NW-Image2.png'
 import Image3 from '@/assets/NW-Image3.png'
@@ -27,119 +26,142 @@ const arrivals: Array<NewArrivalType> = [
     },
 ]
 
-const container={
+const container = {
     hidden: {},
-    visible:{
-        transition: {staggerChildren:0.2}
+    visible: {
+        transition: { staggerChildren: 0.2 }
     }
 }
 
 type Props = {
-    setSelectedPage: (value:SelectedPage) => void;
+    setSelectedPage: (value: SelectedPage) => void;
 }
 
-const NewArrivals = ({setSelectedPage}: Props) => {
-  return (
-    <section id="newarrivals" className='mx-auto min-h-full w-5/6 py-20'>
-        <motion.div
-            onViewportEnter={() => setSelectedPage(SelectedPage.NewArrivals)}
-        >
-            {/* header */}
-            <div className='relative'>
-                <div className='before:absolute before:left-24 before:top-2 before:z-[-1] before:content-newArrivals'>
-                <motion.div
-                    className='md:my-5 md:my-3/5'
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{once:true, amount:0.5}}
-                    transition={{duration:0.5}}
-                    variants={{
-                        hidden:{opacity:0,x:50},
-                        visible:{opacity:1,x:0}
-                    }}
-                >
-                    <HText>NEW ARRIVALS</HText>
-                </motion.div>
-                </div>
-
-            </div>
-
-            {/* new arrivals */}
-            <motion.div 
-                className='md:flex items-center justify-between gap-8 mt-5'
-                initial="hidden"
-                whileInView={"visible"}
-                viewport={{once:true,amount:0.5}}
-                variants={container}
+const NewArrivals = ({ setSelectedPage }: Props) => {
+    return (
+        <section id="newarrivals" className='mb-5' >
+            <motion.div
+                onViewportEnter={() => setSelectedPage(SelectedPage.NewArrivals)}
             >
-                {arrivals.map((arrival:NewArrivalType) => (
-                    <NewArrival 
-                        key={arrival.title}
-                        icon={arrival.icon}
-                        title={arrival.title}
-                        description={arrival.description}
-                    />
-                ))}
-            </motion.div>
+                <div className='mx-auto min-h-full w-5/6 py-20'>
+                    {/* header */}
+                    <div className='relative'>
+                        <div className='before:absolute before:left-24 before:top-2 before:z-[-1] before:content-newArrivals'>
+                            <motion.div
+                                className='md:my-5 md:my-3/5'
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{ once: true, amount: 0.5 }}
+                                transition={{ duration: 0.5 }}
+                                variants={{
+                                    hidden: { opacity: 0, x: 50 },
+                                    visible: { opacity: 1, x: 0 }
+                                }}
+                            >
+                                <HText>NEW ARRIVALS</HText>
+                            </motion.div>
+                        </div>
 
-        {/* grapihs and description */}
-        <div className='mt-16 items-center justify-between gap-20 md:mt-28 md:flex'>
-            {/* graphic */}
-            <img className='mx-auto' src={BenefitsPageGraphic} alt="benefits-page-graphic" />
+                    </div>
 
-            {/* description */}
-            <div>
-                {/* title */}
-                <div className='relative'>
-                    <div className='before:absolute before:-top-20 before:-left-20 before:z-[1] before:content-abstractwaves'>
+                    {/* new arrivals */}
                     <motion.div
-                        className='md:my-5 md:my-3/5'
+                        className='md:flex items-center justify-between gap-8 mt-5'
                         initial="hidden"
-                        whileInView="visible"
-                        viewport={{once:true, amount:0.5}}
-                        transition={{duration:0.5}}
-                        variants={{
-                            hidden:{opacity:0,x:50},
-                            visible:{opacity:1,x:0}
-                        }}
+                        whileInView={"visible"}
+                        viewport={{ once: true, amount: 0.5 }}
+                        variants={container}
                     >
-                        <HText>MILLIONS OF HAPPY MEMBERS GETTING{" "}
-                            <span className='text-primary-500'>FIT</span>
-                        </HText>
+                        {arrivals.map((arrival: NewArrivalType) => (
+                            <NewArrival
+                                key={arrival.title}
+                                icon={arrival.icon}
+                                title={arrival.title}
+                                description={arrival.description}
+                            />
+                        ))}
                     </motion.div>
-                    </div>
-
                 </div>
 
-                {/* description */}
-                <motion.div
-                    className='md:my-5 md:my-3/5'
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{once:true, amount:0.5}}
-                    transition={{delay:0.2,duration:0.5}}
-                    variants={{
-                        hidden:{opacity:0,x:50},
-                        visible:{opacity:1,x:0}
-                    }}
-                >
-                    <p className='my-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora laboriosam enim excepturi consequatur. Reprehenderit eos exercitationem autem dolorem adipisci odit voluptates odio, suscipit magni quibusdam quis voluptatibus maxime minima et.</p>
-                    <p className='mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, voluptates dolorum rerum quidem, minima consequatur quaerat excepturi ducimus earum distinctio facere. At error vitae nostrum animi corporis, reprehenderit qui hic!</p>
-                </motion.div>
+                <div className='min-h-full bg-gradient-yellow'>
 
-                {/* button */}
-                <div className='relative mt-16'>
-                    <div className='before:absolute before-bottom-20 before:right-40 before:z-[-1] before:content-sparkles'>
-                        <ActionButton setSelectedPage={setSelectedPage}>
-                            Join Now
-                        </ActionButton>
+                    <div className="mt-10 justify-between gap-8 md:flex items-center">
+                        <motion.div
+                            className="relative mt-16 basis-2/5 md:mt-0"
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true, amount: 0.5 }}
+                            transition={{ delay: 0.2, duration: 0.5 }}
+                            variants={{
+                                hidden: { opacity: 0, y: 50 },
+                                visible: { opacity: 1, y: 0 },
+                            }}
+                        >
+                            <div className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
+                                <img
+                                    className="w-full"
+                                    alt="contact-us-page-graphic"
+                                    src={SaleImage}
+                                />
+                            </div>
+                        </motion.div>
+
+                        <div className='z-10 md:basis-1/2 sm:p-8'>
+                            {/* headings */}
+                            <motion.div 
+                                className=''
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{once:true, amount:0.5}}
+                                transition={{duration:0.5}}
+                                variants={{
+                                    hidden:{opacity:0,x:-50},
+                                    visible:{opacity:1,x:0}
+                                }}
+                            >
+                                <div className='relative'>
+                                    <div className='before:absolute before:-top-20 before:-left-20 before:z-[-1]'>
+                                        {/* <img src={HomePageText} alt="home-page-text" /> */}
+                                        <div className='text-7xl font-bold'>PAYDAY <br/>
+                                                SALE NOW 
+                                            </div>
+                                    </div>
+                                </div>
+
+                                <p className='mt-8 text-xl'>
+                                Spend minimal $100 get 30% off <br />
+                                voucher code for your next purchase
+                                </p>
+                                <p className='mt-8 text-xl font-bold'>
+                                1 June - 10 June 2021
+                                </p>
+                                <p className='text-xl'>
+                                *Terms & Conditions apply
+                                </p>
+                            </motion.div>
+
+                            {/* actions */}
+                            <motion.div 
+                                className='mt-8 flex items-center gap-8'
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{once:true, amount:0.5}}
+                                transition={{delay:0.2, duration:0.5}}
+                                variants={{
+                                    hidden:{opacity:0,x:-50},
+                                    visible:{opacity:1,x:0}
+                                }}
+                            >
+                                <ActionButton setSelectedPage={setSelectedPage}>Shop Now</ActionButton>
+                            
+                            </motion.div>
+                        </div>
+                        
                     </div>
                 </div>
-            </div>
-        </div>
-        </motion.div>    
-    </section>
-  )
+            </motion.div>
+        </section>
+    )
 }
 
 export default NewArrivals
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
         "gradient-yellowred":
           "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
         "mobile-home": "url('./assets/HomePageGraphic.png')",
+        "gradient-yellow": "linear-gradient(1deg, #E0C340 0.13%, #DFC23E 3.97%, #E1C441 7.2%, #E3C743 10.13%, #E4C542 12.98%, #E6C744 15.99%, #E7C845 19.52%, #E5C643 23.96%, #E6C945 30.43%, #E3C743 36.49%, #E9CA48 42.49%, #EDCE49 49.35%, #F0D44C 55.42%, #F4D84F 61.43%, #F6DA52 65.74%, #F7DB53 72.23%, #F9DD55 77.43%, #F9DF56 83.84%, #FAE157 91.52%, #F9DF56 97.87%)",
       }),
       fontFamily: {
         dmsans: ["DM Sans", "sans-serif"],
