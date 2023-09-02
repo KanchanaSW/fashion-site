f73620997b9d7a15439c2710258cebfbe2110a75
Kanchana Walagambahu <kanchana.walagambahu@axiatadigitallabs.com>
2023-09-02 13:47:09 +0530

    contact us changes done

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,7 +1,6 @@
 import { useForm } from "react-hook-form";
 import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
-import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
 import HText from "@/shared/HText";
 
 type Props = {
@@ -9,8 +8,7 @@ type Props = {
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
-  const inputStyles = `mb-5 w-full rounded-lg bg-primary-300
-  px-5 py-3 placeholder-white`;
+  const inputStyles = `w-3/5 bg-white text-black px-5 py-3 placeholder-black`;
 
   const {
     register,
@@ -26,13 +24,13 @@ const ContactUs = ({ setSelectedPage }: Props) => {
   };
 
   return (
-    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
+    <section id="contact" className="mx-auto h-3/4 flex items-center justify-center pt-24 pb-32 bg-[#E5C643]">
       <motion.div
-        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+        onViewportEnter={() => setSelectedPage(SelectedPage.Contact)}
       >
         {/* HEADER */}
         <motion.div
-          className="md:w-3/5"
+          className="basis-1/2 text-center"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
@@ -43,17 +41,15 @@ const ContactUs = ({ setSelectedPage }: Props) => {
           }}
         >
           <HText>
-            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
+            <span className="text-white text-4xl">JOIN SHOPPING COMMUNITY TO GET MONTHLY PROMO</span> 
           </HText>
-          <p className="my-5">
-            Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl
-            sapien vel rhoncus. Placerat at in enim pellentesque. Nulla
-            adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
+          <p className="my-5 text-white">
+            Type your email down below and be young wild generation
           </p>
         </motion.div>
 
         {/* FORM AND IMAGE */}
-        <div className="mt-10 justify-between gap-8 md:flex">
+        <div className="mt-10 justify-center gap-8 md:flex">
           <motion.div
             className="mt-10 basis-3/5 md:mt-0"
             initial="hidden"
@@ -66,28 +62,12 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             }}
           >
             <form
+              className="flex justify-center"
               target="_blank"
               onSubmit={onSubmit}
               action="https://formsubmit.co/1671b67b70b11931d4c4b0eb69e6dfde"
               method="POST"
             >
-              <input
-                className={inputStyles}
-                type="text"
-                placeholder="NAME"
-                {...register("name", {
-                  required: true,
-                  maxLength: 100,
-                })}
-              />
-              {errors.name && (
-                <p className="mt-1 text-primary-500">
-                  {errors.name.type === "required" && "This field is required."}
-                  {errors.name.type === "maxLength" &&
-                    "Max length is 100 char."}
-                </p>
-              )}
-
               <input
                 className={inputStyles}
                 type="text"
@@ -105,53 +85,15 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                 </p>
               )}
 
-              <textarea
-                className={inputStyles}
-                placeholder="MESSAGE"
-                rows={4}
-                cols={50}
-                {...register("message", {
-                  required: true,
-                  maxLength: 2000,
-                })}
-              />
-              {errors.message && (
-                <p className="mt-1 text-primary-500">
-                  {errors.message.type === "required" &&
-                    "This field is required."}
-                  {errors.message.type === "maxLength" &&
-                    "Max length is 2000 char."}
-                </p>
-              )}
-
               <button
                 type="submit"
-                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+                className="rounded-lg bg-black px-12 transition duration-500 text-white"
               >
-                SUBMIT
+                Send
               </button>
             </form>
           </motion.div>
 
-          <motion.div
-            className="relative mt-16 basis-2/5 md:mt-0"
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true, amount: 0.5 }}
-            transition={{ delay: 0.2, duration: 0.5 }}
-            variants={{
-              hidden: { opacity: 0, y: 50 },
-              visible: { opacity: 1, y: 0 },
-            }}
-          >
-            <div className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
-              <img
-                className="w-full"
-                alt="contact-us-page-graphic"
-                src={ContactUsPageGraphic}
-              />
-            </div>
-          </motion.div>
         </div>
       </motion.div>
     </section>
